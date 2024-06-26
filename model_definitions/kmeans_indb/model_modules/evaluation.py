7c2b461b63afd258a05c93eeaaa632ab2c9332e3
TatianaBokareva <tbokareva@gmail.com>
2024-06-27 07:44:27 +1000

    Update evaluation.py

--- model_definitions/kmeans_indb/model_modules/evaluation.py
+++ model_definitions/kmeans_indb/model_modules/evaluation.py
@@ -5,23 +5,4 @@ from teradataml import *
 
 
 def evaluate(context: ModelContext, **kwargs):
-    """
-    Performs evaluation of a model using test data, plot points and created bar chars
-
-    Parameters:
-    - context (ModelContext): The model context object containing configuration and paths.
-
-    Keyword Arguments:
-    - kwargs: Miscellaneous keyword arguments.
-
-    Returns:
-    - None
-    """
-    aoa_create_context()  # Set up the AOA context for evaluation
-    scored_tdf = DataFrame(in_schema("demo_user","kmean_score"))
-    train_tdf =  DataFrame(in_schema("demo_user","kmean_score"))
-   
-    #tdf1 = tdf.join(other = KMeansPredict_out.result
-     #       , on = ["id=id"], how = "inner", lprefix = "t1")
-    #obj = Antiselect(data=tdf1,
-    #                 exclude=['t1_id'])
+  print("All done as there is not need to exaluate kmean")
