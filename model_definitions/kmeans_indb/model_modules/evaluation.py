aab220856f8273e761c97718a3b6929e8e382818
TatianaBokareva <tbokareva@gmail.com>
2024-06-27 07:48:54 +1000

    Update evaluation.py

--- model_definitions/kmeans_indb/model_modules/evaluation.py
+++ model_definitions/kmeans_indb/model_modules/evaluation.py
@@ -2,7 +2,11 @@
 import matplotlib.pyplot as plt
 import teradataml as tdml
 from teradataml import *   
-
+from aoa import (
+    record_scoring_stats,
+    aoa_create_context,
+    ModelContext
+)
 
 def evaluate(context: ModelContext, **kwargs):
   print("All done as there is not need to exaluate kmean")
