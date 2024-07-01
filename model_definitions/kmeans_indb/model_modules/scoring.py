44acaf44988c3f04dd2226f888624ac2ec6d097b
TatianaBokareva <tbokareva@gmail.com>
2024-07-01 19:30:11 +1000

    Update scoring.py

--- model_definitions/kmeans_indb/model_modules/scoring.py
+++ model_definitions/kmeans_indb/model_modules/scoring.py
@@ -39,13 +39,13 @@ def score(context: ModelContext, **kwargs):
     KMeansPredict_out = KMeansPredict(object=Kmean_out,data=tdf).result
     print(KMeansPredict_out)
     copy_to_sql(df = KMeansPredict_out, table_name = "kmeans_tmp", primary_index="id", if_exists="replace")
-    Km = DataFrame(in_schema(context.dataset_info.predictions_database,"kmeans_tmp"))
+    Km = DataFrame(in_schema(context.dataset_info.predictions_database,"kmeans_tmp")).to_pandas()
     print(Km)
     # Convert predictions to pandas DataFrame, adjust the column name to target_name, and ensure the type is integer
     # Retrieve target, and entity key names from the model context
     target_name = context.dataset_info.target_names
     entity_key = context.dataset_info.entity_key
-    predictions_pdf = Km.to_pandas(all_rows=True).rename(columns={"td_clusterid_kmeans": target_name}).astype({target_name: int})
+    predictions_pdf = Km.rename(columns={"td_clusterid_kmeans": target_name}).astype({target_name: int})
 
     print("Finished Scoring")
     # Prepare the predictions DataFrame for database insertion
