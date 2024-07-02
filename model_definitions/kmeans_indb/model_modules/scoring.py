6d8ab65491686964f5e029c50e8123b57b7f0b73
TatianaBokareva <tbokareva@gmail.com>
2024-07-02 11:24:43 +1000

    Update scoring.py

--- model_definitions/kmeans_indb/model_modules/scoring.py
+++ model_definitions/kmeans_indb/model_modules/scoring.py
@@ -39,9 +39,9 @@ def score(context: ModelContext, **kwargs):
     KMeansPredict_out = KMeansPredict(object=Kmean_out,data=tdf).result
     print("Output of scoring")
     print(KMeansPredict_out)
-    print(KMeansPredict_out)
+
     # Retrieve target, and entity key names from the model context. Note: order columns to match the expected schema in the database
-    KMeansPredict_out.assign(drop_columns=True
+    KMeansPredict_out = KMeansPredict_out.assign(drop_columns=True
                              # Add job_id to track the execution
                             ,job_id = context.job_id 
                              # Set entity key from the features_pdf
